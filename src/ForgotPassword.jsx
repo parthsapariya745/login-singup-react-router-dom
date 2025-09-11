fec49930596dcbc50fd30308543c9a9c5f2f646c
parthsapariya745 <parthsapariyait7@gmail.com>
2025-09-11 13:44:36 +0530

    update

--- src/ForgotPassword.jsx
+++ src/ForgotPassword.jsx
@@ -4,18 +4,18 @@ import { useNavigate } from "react-router-dom"
 const ForgotPassword = () => {
   let [newPassword, setNewPassword] = useState("")
   let [confirmPassword, setConfirmPassword] = useState("")
-  
+
   let navigate = useNavigate()
 
   let handlePassword = () => {
     if (newPassword === confirmPassword) {
-        alert("Password change successfully")
-        navigate("/Login")
+      alert("Password change successfully")
+      navigate("/Login")
 
-        localStorage.setItem("password", JSON.stringify(newPassword))
+      localStorage.setItem("password", JSON.stringify(newPassword))
     }
     else {
-        alert("Set password and Confirm password are not same")
+      alert("Set password and Confirm password are not same")
     }
     setNewPassword("")
     setConfirmPassword("")
@@ -24,8 +24,8 @@ const ForgotPassword = () => {
     <div className="container">
       <div className="login">
         <h1>Reset Password</h1>
-        <input type="password" onChange={(e)=> setNewPassword(e.target.value)} value={newPassword} placeholder="Set new password" />
-        <input type="password" onChange={(e)=> setConfirmPassword(e.target.value)} value={confirmPassword} placeholder="Confirm Password" />
+        <input type="password" onChange={(e) => setNewPassword(e.target.value)} value={newPassword} placeholder="Set new password" />
+        <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword} placeholder="Confirm Password" />
         <button onClick={handlePassword}>Change Password</button>
       </div>
     </div>
